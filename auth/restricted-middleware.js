94920fd94b343491d50a3bac9c4d1f70a4aa2beb
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-14 22:14:18 -0500

    made use of cookie in middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,22 +1,8 @@
-const express = require('express')
-const router = express.Router();
-const App = require('../api/app-model');
-
-const bcrypt = require('bcryptjs');
-
-
 module.exports = function restricted(req, res, next) {
     const { username, password } = req.headers;
-
-    App.findBy({ username })
-        .then(userLogin => {
-            if(userLogin && bcrypt.compareSync(password, userLogin.password)){
-                res.status(201).json({ message: `Welcome to the ${userLogin.username} castle.` })
-            } else {
-                res.status(401).json({ message: 'invalid credentials' });
+    if(req.session && req.session.user) {
+        next();
+    } else {
+                res.status(401).json({ message: 'You shall not pass!' });
             }
-        })
-        .catch(err => {
-            res.status(500).json({ message: "please provide valid credentials" });
-        });
-};
+ };
