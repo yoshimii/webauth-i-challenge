c0937705b1ba6a32c806a5845294d783b35e6ed1
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-15 22:07:21 -0500

    form submits handled and clear state function added

--- cool-club/src/App.js
+++ cool-club/src/App.js
@@ -1,10 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, setState } from 'react';
+import Axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+import UsersList from './users/UsersList';
+import User from './users/User';
+import UserList from './users/UsersList';
 
 function App() {
   const [logUser, setLogin] = useState({username:'', password:''});
   const [regUser, setRegister] = useState({username:'', password:''});
+  const [users, setUsers] = useState()
   
   const handleRegisterChange = e => {
     setRegister({ ...regUser, [e.target.name]: e.target.value })
@@ -14,14 +20,38 @@ function App() {
     setLogin({ ...logUser, [e.target.name]: e.target.value })
   }
 
+
+  const clearState = e => {
+    setRegister({username: '', password: ''})
+    setLogin({username: '', password: ''})
+  }
   const registerUser = e => {
     e.preventDefault();
-    axios.post('http://localhost')
+    Axios.post('http://localhost:4000/api/register', regUser)
+      .then(_=> clearState())
+      .then(_=> <UserList/>)
+      .then(user => console.log(`successful registration`))
+      .catch(err => {
+        'no user has been registered'
+      })
   }
+
+  const loginUser = e => {
+    e.preventDefault();
+    Axios.post('http://localhost:4000/api/login', logUser)
+      .then(_=> <UserList/>)
+      .then(_=> clearState())
+      .then(user=> console.log(`user ${logUser.username} has successfully logged in.`))
+      .catch(err => {
+        'no user has been logged in'
+      })
+  }
+
+  
 
   return (
     <div className="App">
-      <h2>Registered Users</h2>,
+      <h2>Registered Users</h2>
       <form onSubmit={registerUser}>
         <input
         type='text'
@@ -39,15 +69,15 @@ function App() {
         name='password'
         >
         </input>
-        <button>Register</button>
+        <button><Link to="/users">Register</Link></button>
         
-      </form>,
+      </form>
       <form onSubmit={loginUser}>
       <input
         type='text'
         placeholder='enter a username'
         value={logUser.username}
-        onChange={handleRegisterChange}
+        onChange={handleLoginChange}
         name='username'
         >
         </input>
@@ -59,8 +89,9 @@ function App() {
         name='password'
         >
           </input>
-        <button>Log in</button>
+        <button><Link to="/users">Log in</Link></button>
       </form>
+      <Route path='/users' component={UsersList}/>
     </div>
   );
 }
